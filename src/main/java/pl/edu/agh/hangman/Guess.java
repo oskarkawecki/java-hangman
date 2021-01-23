ccc4cade1d232f67410dc23aa45f714b05bfafb6
Lukasz <korzen1984@gmail.com>
2021-01-23 11:33:54 +0100

    guess

--- src/main/java/pl/edu/agh/hangman/Guess.java
+++ src/main/java/pl/edu/agh/hangman/Guess.java
@@ -1,5 +1,32 @@
 package pl.edu.agh.hangman;
+import java.util.Scanner;
 
 public class Guess {
-
+	
+	string letter;
+		
+	
+	
+	System.out.println("Guess letter");
+	
+	 Scanner consoleIn = new Scanner(System.in);
+	 Scanner letter = new Scanner(System.in);
+	 
+	 
+	 String letter = letter.next
+	 
+	 
+	 System.out.println("Zgadnij litere");
+	 litera = litera.nextLine();
+	 
+	 
+	 if litera 
 }
+
+
+
+else
+	hangman
+
+	
+	System.out.println("Udalo sie!!");
