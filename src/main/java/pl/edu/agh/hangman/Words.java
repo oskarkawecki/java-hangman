d40daa923ea096cb1e6e72e7d8ec2dc21d265eca
Oskar Kawecki <oskar.kawecki@schibsted.com>
2021-01-23 11:18:00 +0100

    Second

--- src/main/java/pl/edu/agh/hangman/Words.java
+++ src/main/java/pl/edu/agh/hangman/Words.java
@@ -1,19 +1,34 @@
-package pl.edu.agh.hangman;
-
-import java.io.File;
-import java.util.Collection;
-import java.util.Scanner;
-
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Words {
 
-	public static Collection getWords() {
-		// TODO Auto-generated method stub
-		return null;
+	private ArrayList<String> words;
+
+	public ArrayList<String> getWords() {
+		return words;
 	}
-	
-	
+
+	public void setWords(ArrayList<String> words) {
+		this.words = words;
+	}
+
+	public static void constructor(String slowa) throws IOException {
+
+		ArrayList words = new ArrayList();
+		BufferedReader read = new BufferedReader(new FileReader("slowa"));
+		String line = read.readLine();
+
+
+		while (line != null) {
+			words.add(line);
+			// line = reader.readline();
+		}
+	}
+
+
 }
 
 
