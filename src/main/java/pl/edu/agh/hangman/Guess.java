d8efe1926a38f08835bcd36bbc1636d7dfaf08b9
Orannis789 <rozbicki.lukas@gmail.com>
2021-01-23 11:34:38 +0100

    words i draw

--- src/main/java/pl/edu/agh/hangman/Guess.java
+++ src/main/java/pl/edu/agh/hangman/Guess.java
@@ -2,4 +2,10 @@ package pl.edu.agh.hangman;
 
 public class Guess {
 
+	
+	
+	
+	int ammountOfGuesses = randomWordToGuess.length; //total tries to guess a word.
+    char playerGuess[] = new char[ammountOfGuesses];
+    
 }
--- src/main/java/pl/edu/agh/hangman/Words.java
+++ src/main/java/pl/edu/agh/hangman/Words.java
@@ -1,19 +1,29 @@
 package pl.edu.agh.hangman;
 
-import java.io.File;
-import java.util.Collection;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+public class Words{
 
+  private static ArrayList<String> words;
 
-
-public class Words {
-
-	public static Collection getWords() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
+  public static ArrayList<String> getWords() {
+	return words;
 }
 
+  public void setWords(ArrayList<String> words) {
+	this.words = words;
+}
+  public static void addWordsToTheList() throws IOException{
+
+  ArrayList<String> words = new ArrayList<>();
+  BufferedReader read = new BufferedReader(new FileReader("arc/main.resources/slowa.txt"));
+  String line = read.readLine();
+
+
+    while (line != null){
+      words.add(line);
+      //line = reader.readline();
+    }
+  }
+}
 
