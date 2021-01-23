0e45dbcc087649a041dfbebe9dce6256b76fc400
Orannis789 <rozbicki.lukas@gmail.com>
2021-01-23 10:52:05 +0100

    Draw

--- src/main/java/pl/edu/agh/hangman/Draw.java
+++ src/main/java/pl/edu/agh/hangman/Draw.java
@@ -1,5 +1,20 @@
 package pl.edu.agh.hangman;
 
+import java.util.Random;
+
 public class Draw {
+	
+	private char[] draw(String [] words) {
+		
+		words = (String[]) Words.getWords().toArray();
+		
+		Random random = new Random();
+		
+		 int randomNumber = random.nextInt(words.length); //random.nextInt(10);
+         char randomWordToGuess[] = words[randomNumber].toCharArray();
+         
+		return randomWordToGuess;	
+		
+	}
 
 }
--- src/main/java/pl/edu/agh/hangman/Words.java
+++ src/main/java/pl/edu/agh/hangman/Words.java
@@ -1,5 +1,19 @@
 package pl.edu.agh.hangman;
 
+import java.io.File;
+import java.util.Collection;
+import java.util.Scanner;
+
+
+
 public class Words {
 
+	public static Collection getWords() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 }
+
+
